0e6ab2d3dc9fca30d8fe214aaeffbaf636a27a4a
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-14 06:45:49 +0300

    stage0-module3-task3

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,15 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+       int first = 1;
+       int second = 10;
+       int third = 100;
+       System.out.println(first);
+       System.out.println(second);
+       System.out.println(third);
     }
 }
+
+//Create 3 int variables with names: first, second, third.
+// Initialize them with values: 1, 10, 100.
+// Make console outputs for all of them, each on new line.
--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,6 +2,9 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int a,b,c = 10;
+        System.out.println(c);
     }
 }
+//Declare 3 int variables within one statement and call them a, b, c (order must be followed).
+// Initialize them in one statement with value "10". Print to console "c".
